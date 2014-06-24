cd1c267f531efd6b03910e9f5ce66664d02de6e5
Bryan Tong <contact@nullivex.com>
2014-06-24 15:18:11 -0600

    basic query working

---
+++ example.js
@@ -0,0 +1,27 @@
+'use strict';
+var rwhois = require('./index')
+var app = rwhois({debug: true})
+
+//handle ipv4 queries
+app.query('ipv4',function(req,next){
+  console.log(req)
+  next(null,[
+    'network:ID:NET-IBMNET-3.0.0.0/0',
+    'network:Auth-Area:0.0.0.0/0',
+    'network:Network-Name:IBMNET-3',
+    'network:IP-Network:123.45.67.0/24',
+    'network:Org-Name:IBM',
+    'network:Street-Address:1234 Maneck Avenue',
+    'network:City:Black Plains',
+    'network:State:NY',
+    'network:Postal-Code:12345',
+    'network:Country-Code:US',
+    'network:Tech-Contact;I:MG305.COM',
+    'network:Updated:19931120123455000',
+    'network:Updated-By:joeblo@nic.ddn.mil',
+    'network:Class-Name:network'
+  ])
+})
+
+//setup and listen
+app.listen()
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 /**
  * Main module
  * @type {exports}
